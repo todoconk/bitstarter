6f6e0b217da8b4faefd7b17cb6394e8a15fc7440
TodoConK <todoconk@gmail.com>
2013-07-10 06:16:52 +0000

    Reading index.html in web.js

--- web.js
+++ web.js
@@ -4,9 +4,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var index = fs.readFileSync('index.html');
+var buffer = new Buffer(index, "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(index);
+  response.send(buffer.toString("utf-8", 0, 12));
 });
 
 var port = process.env.PORT || 5000;
